e404772b967f85e86cc7f4be7bbf5764f8abc3e4
Aayush <falcon.bridge2000@gmail.com>
2022-01-25 17:54:16 +0530

    added functionalty to open 5 tabs and open the google meet in all of the with 5 different accounts on same profile

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const form = document.querySelector("form");
-
+const inp = document.querySelector("input");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  window.open("https://meet.google.com/jmf-tynd-ino?authuser=1", "_blank");
+  window.open("https://meet.google.com/jmf-tynd-ino?authuser=2", "_blank");
+  window.open("https://meet.google.com/jmf-tynd-ino?authuser=3", "_blank");
+  window.open("https://meet.google.com/jmf-tynd-ino?authuser=4", "_blank");
+  window.open("https://meet.google.com/jmf-tynd-ino?authuser=5", "_blank");
 });
