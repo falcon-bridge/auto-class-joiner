14170f7975f0f224116816140d21d185072b2216
Aayush <falcon.bridge2000@gmail.com>
2022-01-25 18:40:44 +0530

    added an input field to take number of participants as dynamic value and has given it a default value of 5

--- app.js
+++ app.js
@@ -3,18 +3,14 @@ const openTab = (link, index) => {
 };
 
 const form = document.querySelector("form");
-const inp = document.querySelector("input");
+
 form.addEventListener("submit", (event) => {
+  const link = document.getElementById("link").value;
+  const noOfUsers = +document.getElementById("noOfUsers").value;
+
   event.preventDefault();
-  const link = inp.value;
-  console.log(typeof link);
   console.log(link);
-  for (let i = 1; i <= 5; i++) {
+  for (let i = 1; i <= noOfUsers; i++) {
     openTab(link, i);
   }
-  //   window.open("https://meet.google.com/jmf-tynd-ino?authuser=1", "_blank");
-  //   window.open("https://meet.google.com/jmf-tynd-ino?authuser=2", "_blank");
-  //   window.open("https://meet.google.com/jmf-tynd-ino?authuser=3", "_blank");
-  //   window.open("https://meet.google.com/jmf-tynd-ino?authuser=4", "_blank");
-  //   window.open("https://meet.google.com/jmf-tynd-ino?authuser=5", "_blank");
 });
