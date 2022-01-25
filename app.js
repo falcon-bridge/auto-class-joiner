d85955565af2c45d8367cab1c47523755517f021
Aayush <falcon.bridge2000@gmail.com>
2022-01-25 19:47:17 +0530

    changed the logic quite a bit according to a specific user

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   const link = document.getElementById("link").value;
   const noOfUsers = +document.getElementById("noOfUsers").value;
+  const participant = document.querySelector("select").value;
 
   event.preventDefault();
-  console.log(link);
-  for (let i = 1; i <= noOfUsers; i++) {
-    openTab(link, i);
+  if (participant === "Rishabh") {
+    for (let i = 1; i <= noOfUsers + 1; i++) {
+      if (i !== 2) {
+        openTab(link, i);
+      }
+    }
+  } else {
+    for (let i = 1; i <= noOfUsers; i++) {
+      openTab(link, i);
+    }
   }
 });
