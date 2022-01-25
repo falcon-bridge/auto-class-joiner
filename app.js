ff901a40e0f434954d222033a0d9fee9c7f561be
Aayush <falcon.bridge2000@gmail.com>
2022-01-25 18:04:40 +0530

    made a function to open new tabs with given meet link with different accounts and called it required number of times with the help of a loop

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+const openTab = (link, index) => {
+  window.open(`${link}?authuser=${index}`, "_blank");
+};
+
 const form = document.querySelector("form");
 const inp = document.querySelector("input");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  window.open("https://meet.google.com/jmf-tynd-ino?authuser=1", "_blank");
-  window.open("https://meet.google.com/jmf-tynd-ino?authuser=2", "_blank");
-  window.open("https://meet.google.com/jmf-tynd-ino?authuser=3", "_blank");
-  window.open("https://meet.google.com/jmf-tynd-ino?authuser=4", "_blank");
-  window.open("https://meet.google.com/jmf-tynd-ino?authuser=5", "_blank");
+  const link = inp.value;
+  console.log(typeof link);
+  console.log(link);
+  for (let i = 1; i <= 5; i++) {
+    openTab(link, i);
+  }
+  //   window.open("https://meet.google.com/jmf-tynd-ino?authuser=1", "_blank");
+  //   window.open("https://meet.google.com/jmf-tynd-ino?authuser=2", "_blank");
+  //   window.open("https://meet.google.com/jmf-tynd-ino?authuser=3", "_blank");
+  //   window.open("https://meet.google.com/jmf-tynd-ino?authuser=4", "_blank");
+  //   window.open("https://meet.google.com/jmf-tynd-ino?authuser=5", "_blank");
 });
