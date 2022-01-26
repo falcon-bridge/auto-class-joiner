a20e685f8ab05e6dc98cfa160ea5006b2338cd39
Aayush <falcon.bridge2000@gmail.com>
2022-01-26 15:08:32 +0530

    refactored the code and avoided duplicacy o code

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
-const openTab = (link, index) => {
+const form = document.querySelector("form");
+
+const openLink = (link, index) => {
   window.open(`${link}?authuser=${index}`, "_blank");
 };
 
-const openCodeTab = (code, index) => {
+const openCode = (code, index) => {
   window.open(`http://meet.google.com/${code}?authuser=${index}`, "_blank");
 };
 
-const form = document.querySelector("form");
-
-form.addEventListener("submit", (event) => {
-  const link = document.getElementById("link").value;
-  const noOfUsers = +document.getElementById("noOfUsers").value;
-  const participant = document.querySelector("select").value;
-  const code = document.getElementById("code").value;
-
-  event.preventDefault();
+const proceed = (participant, proceedFn, meetInfo, noOfUsers) => {
   if (participant === "Rishabh") {
     for (let i = 1; i <= noOfUsers + 1; i++) {
       if (i !== 2) {
-        if (link.trim.length !== 0) {
-          openTab(link, i);
-        } else {
-          openCodeTab(code, i);
-        }
+        proceedFn(meetInfo, i);
       }
     }
-  } else if (link.trim.length !== 0) {
-    for (let i = 1; i <= noOfUsers; i++) {
-      openTab(link, i);
+  } else if (participant === "Aayush") {
+    for (let i = 2; i <= noOfUsers + 1; i++) {
+      proceedFn(meetInfo, i);
     }
   } else {
     for (let i = 1; i <= noOfUsers; i++) {
-      openCodeTab(code, i);
+      proceedFn(meetInfo, i);
     }
+  }
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const participant = document.getElementById("participant").value;
+  const option = document.getElementById("option").value;
+  const meetInfo = document.getElementById("meetInfo").value;
+  const noOfUsers = +document.getElementById("noOfUsers").value;
+
+  if (option === "link") {
+    proceed(participant, openLink, meetInfo, noOfUsers);
+  } else {
+    proceed(participant, openCode, meetInfo, noOfUsers);
   }
 });
